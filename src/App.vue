453145d00c9e5c4d5dcfb8304925727335b750d7
Амир Нигматуллаев <@itstep.uz>
2023-10-08 20:30:44 +0300

    feat: add a headbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
-<script setup></script>
+<script setup>
+import Header from "./components/Header.vue";
+</script>
 
 <template>
-  <p>Current route name: {{ $route.name }}</p>
-  <ul>
+  <Header></Header>
+  <!-- <ul>
     <li><router-link :to="{ name: 'home' }">Home</router-link></li>
     <li><router-link :to="{ name: 'addquiz' }">Create</router-link></li>
-  </ul>
+  </ul> -->
   <router-view class="view"></router-view>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 <template>
   <div>
+    <Header></Header>
     <h1>Home page</h1>
   </div>
 </template>
